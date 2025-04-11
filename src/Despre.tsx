1cb2e4e40a8adcb900e158d04e590877c33155d4
MurfyChe <razvan.mortan2006@gmail.com>
2025-04-11 22:13:41 +0300

    Update Despre.tsx

--- src/Despre.tsx
+++ src/Despre.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Despre.css";
 
 function Despre() {
@@ -6,19 +5,13 @@ function Despre() {
     <div className="despre-section">
       <h2>Despre Noi</h2>
       <p>
-      Alifirium este mai mult decât un brand; este un angajament față de sănătatea și bunăstarea ta. Cu o pasiune profundă pentru natură și pentru beneficiile pe care aceasta le oferă, ne dedicăm să creăm produse de înaltă calitate care să răspundă nevoilor tale specifice.
+        Alifirium este mai mult decât un brand; este un angajament față de sănătatea și bunăstarea ta. Cu o pasiune profundă pentru natură și pentru beneficiile pe care aceasta le oferă, ne dedicăm să creăm produse de înaltă calitate care să răspundă nevoilor tale specifice.
       </p>
       <p>
-      Fiecare alifie pe care o producem este rezultatul unui proces atent și meticulos, începând de la selecția
-       ingredientelor și până la ambalarea produsului finit. Suntem mândri să folosim exclusiv ingrediente naturale, 
-       cultivate cu grijă și recoltate la momentul potrivit pentru a-și păstra proprietățile terapeutice. 
-       Aceste ingrediente sunt combinate cu tehnici tradiționale și cunoștințe moderne, pentru a asigura eficiență și siguranță în utilizare.
+        Fiecare alifie pe care o producem este rezultatul unui proces atent și meticulos, începând de la selecția ingredientelor și până la ambalarea produsului finit. Suntem mândri să folosim exclusiv ingrediente naturale, cultivate cu grijă și recoltate la momentul potrivit pentru a-și păstra proprietățile terapeutice. Aceste ingrediente sunt combinate cu tehnici tradiționale și cunoștințe moderne, pentru a asigura eficiență și siguranță în utilizare.
       </p>
       <p>
-      Ne dorim să fim partenerul tău de încredere în menținerea unei vieți sănătoase, oferindu-ți produse care nu doar că susțin
-       sănătatea fizică, dar contribuie și la echilibrul și starea ta generală de bine. 
-       De la alifii pentru calmarea durerilor musculare, până la cele care îngrijesc pielea delicată sau iritată, 
-       fiecare formulă este creată cu scopul de a-ți aduce confort și alinare.
+        Ne dorim să fim partenerul tău de încredere în menținerea unei vieți sănătoase, oferindu-ți produse care nu doar că susțin sănătatea fizică, dar contribuie și la echilibrul și starea ta generală de bine. De la alifii pentru calmarea durerilor musculare, până la cele care îngrijesc pielea delicată sau iritată, fiecare formulă este creată cu scopul de a-ți aduce confort și alinare.
       </p>
     </div>
   );
